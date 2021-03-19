03b59517819b2c90eb2ef91f581971ba0960f5ff
DPostnik <DPostnik>
2021-03-19 10:21:08 +0300

    updated display component add styles and binding

--- src/app/components/display/display.component.ts
+++ src/app/components/display/display.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-display',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DisplayComponent implements OnInit {
 
+  @Input() value: string;
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
 
 }
