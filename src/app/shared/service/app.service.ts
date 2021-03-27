98b4370b5c500bd462de55587aed1dd354adbca1
DPostnik <DPostnik>
2021-03-28 00:27:16 +0300

    add comments for logic

--- src/app/shared/service/app.service.ts
+++ src/app/shared/service/app.service.ts
@@ -4,13 +4,6 @@ import {Injectable} from "@angular/core";
   providedIn:'root'
 })
 export class AppService {
-  private symbolsForNumbers = [
-    '+/-', '%', 'M-', 'M+', '+', '=', 'x'
-  ];
-
-  private operators= [
-    '+', '=', 'x' , '-', '/'
-  ]
 
   public symbols = [
     'OFF', '+/-', '√', '%', 'MRC', 'M-', 'M+', '/', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='
@@ -37,7 +30,7 @@ export class AppService {
   private firstAction = false;
 
   handleData(value: string){
-    if(value == 'OFF'){
+    if(value == 'OFF'){ // выключаем + очищаем
       this.num1f = false;
       this.num1 = '';
       this.memoryValue = '';
@@ -49,17 +42,17 @@ export class AppService {
       return '';
     }
 
-    if(value != 'MRC'){
+    if(value != 'MRC'){ // если не mrc очищаем счётчик
       this.counterMRC = 0;
     }
-    if(this.numbers.includes(value)) {
-      if(this.firstAction){
+    if(this.numbers.includes(value)) { // если цифарка
+      if(this.firstAction){ // если действие первое(после =)
        this.num1 = '';
        this.num1f = false;
        this.firstAction = false;
       }
-        if (this.num1f) {
-          if (this.op1 && value == '.' && this.num2 == '') {
+        if (this.num1f) { // если первое число записано
+          if (this.op1 && value == '.' && this.num2 == '') { //если . то добавляем 0
             this.num2 = '0' + value;
           } else {
             this.num2 += value;
@@ -73,22 +66,22 @@ export class AppService {
         }
     }else{
       this.firstAction = false;
-      if(this.selfOperations.includes(value)){
+      if(this.selfOperations.includes(value)){ // операции проводимые к числу к которому применяются
         if(this.num2){
-          this.num2 = this.handleSelfOperations(value, this.num2);
+          this.num2 = this.handleSelfOperations(value, this.num2);//если второе число записано то к нему
         }else{
           this.num1 = this.handleSelfOperations(value, this.num1);
           this.num1f = false;
         }
       }else{
         if(value == 'MRC'){
-          if(this.counterMRC === 1)
+          if(this.counterMRC === 1) // если второй нажато очищается
           {
             this.memoryValue = '';
             this.counterMRC = 0;
             this.num1 = '';
           }else{
-            if(!this.op){
+            if(!this.op){ // если нету оператора то в первое иначе во второе
               this.num1 = this.memoryValue;
             }
             else{
@@ -100,22 +93,22 @@ export class AppService {
             }
             this.counterMRC++;
           }
-        }else{
-          if(!this.op){
+        }else{ // если не mrc
+          if(!this.op){ //если нету оператора, записываем его и показываем окончание первого числа
             this.op = value;
             this.num1f = true;
             this.op1 = true;
           }else{
-            switch(value){
+            switch(value){ // иначе выполняем преобразование и записываем результат в первое число
               case '=':{
                 value='';
                 this.count(value);
                 this.op1 = false;
-                this.firstAction = true;
+                this.firstAction = true; // тут показываем что наше число можно изменить на другое(типо если мы будем исать другое число)
                 break;
               }
               case 'x':{
-                value='*';
+                value='*';//символ на клаве х становится * в выражение
               }
               default:{
                 this.count(value);
@@ -126,7 +119,7 @@ export class AppService {
       }
     }
     let str = '';
-    if(this.num1!==''){
+    if(this.num1!==''){ // добавляем в результирующиую строку потиху символы
       str+=this.num1;
       if(this.op){
           str+=this.op;
@@ -142,7 +135,7 @@ export class AppService {
   }
 
   count(value){
-    if(!this.op1){
+    if(!this.op1){ // если сюда придёт один операнд и один оператор, то выходим
       this.op = value;
       this.op1 = true;
     }else{
@@ -160,7 +153,7 @@ export class AppService {
   handleSelfOperations(value, temp){
     switch(value){
       case '+/-':{
-        if(temp[0]=='-'){
+        if(temp[0]=='-'){ // приводим число к его -(х)
           let numb = eval(temp);
           numb = -numb;
           temp = numb.toString();
@@ -168,19 +161,19 @@ export class AppService {
         temp = '-' + temp;
         break;
       }
-      case 'M-':{
+      case 'M-':{ // вычитаем из числа(в нашей памяти) последнее число на экране и обнуляем последнее число
         let numb = +this.memoryValue;
         numb -= +temp;
         this.memoryValue = numb.toString();
         return '';
       }
-      case 'M+':{
+      case 'M+':{ // добавляем к числу(в нашей памяти) последнее число на экране и обнуляем последнее число
         let numb = +this.memoryValue;
         numb += +temp;
         this.memoryValue = numb.toString();
         return '';
       }
-      default: {
+      default: { // Берём корень нашего последнего числа
         temp = Math.sqrt(eval(temp));
       }
     }
